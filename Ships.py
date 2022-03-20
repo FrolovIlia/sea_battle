004d9d0f5865f2f1ed14b585c385c0086f0516cd
FrolovIlia <85966869+FrolovIlia@users.noreply.github.com>
2022-03-20 16:05:12 +0300

    Add subclasses in class Ship. Add method draw_field in class Canva.

--- Ships.py
+++ Ships.py
@@ -1,2 +1,29 @@
+class Canva:
+    def draw_field(self):
+        pass
+
 class Ships:
+    class Carrier:
+        pass
+
+    class BattleShip:
+        pass
+
+    class Cruiser:
+        pass
+
+    class Submarine:
+        pass
+
+    class Destroyer:
+        pass
+
+
+class Players_Stat:
     pass
+
+class HitShips:
+    pass
+
+
+
