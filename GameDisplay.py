69ea31bcff4aaa1b3c5feb809ad9daa2015a7647
FrolovIlia <85966869+FrolovIlia@users.noreply.github.com>
2022-04-11 16:32:00 +0300

    Separate constant variables.

--- GameDisplay.py
+++ GameDisplay.py
@@ -5,6 +5,9 @@ from GameLogic import dead_ships
 display_size_x = 840
 display_size_y = 600
 
+small_xy_hit_size = display_size_y / 30
+ship_size = (display_size_x / 14, display_size_y / 3)
+
 pygame.init()
 game_display = pygame.display.set_mode((display_size_x, display_size_y))
 pygame.display.set_caption('BattleShip')
@@ -20,15 +23,20 @@ game_display.blit(sign_counter, (85, 20))
 player_name = hit_counter_1.render('Player 1', True, (180, 0, 0))
 game_display.blit(player_name, (50, 50))
 
-# Параметры кораблей
-aircraft_shape = pygame.transform.scale(pygame.image.load('pictures/Aircraft Shape.png'), (90, 30))
-game_display.blit(aircraft_shape, (display_size_x / 14, display_size_y / 3))
+# Параметры кораблей и вспомогательных элементов
 
-empty_cell = pygame.transform.scale(pygame.image.load('pictures/m_Hit small.png'), (display_size_y/30, display_size_y/30))
-game_display.blit(empty_cell, (140, 110))
+carrier_shape = pygame.transform.scale(pygame.image.load('pictures/Carrier_shape.png'), (120, 30))
+game_display.blit(carrier_shape, ship_size)
 
-padded_cell = pygame.transform.scale(pygame.image.load('pictures/m_Miss small.png'), (20, 20))
-game_display.blit(padded_cell, (160, 110))
+empty_cell = pygame.transform.scale(pygame.image.load('pictures/m_Hit small.png'), (small_xy_hit_size, small_xy_hit_size))
+game_display.blit(empty_cell, (180, display_size_y / 3))
+
+padded_cell = pygame.transform.scale(pygame.image.load('pictures/m_Miss small.png'), (small_xy_hit_size, small_xy_hit_size))
+game_display.blit(padded_cell, (200, display_size_y / 3))
+
+
+
+
 
 
 def event_handler():
