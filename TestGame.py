c0da5d6c6ebeea05c407c39ab26c95f7f77ea116
FrolovIlia <85966869+FrolovIlia@users.noreply.github.com>
2022-04-16 20:51:41 +0300

    Create unit test module with training methods.

--- TestGame.py
+++ TestGame.py
@@ -2,13 +2,13 @@ import unittest
 import Sandbox
 
 
-
 class TestCalc(unittest.TestCase):
     def test_add(self):
         self.assertEqual(Sandbox.add(3, 6), 9)
 
     def test_is_positive(self):
         self.assertTrue(Sandbox.is_positive(1))
+
 
 if __name__ == '__main__':
     unittest.main()
